45d86ccc353d03f2e74a2053d81ce1a568d45cbb
Thomas Lekanger <datagutt@lekanger.no>
2021-02-19 15:10:28 +0100

    guac: Minor fix

--- scrapers/guac.js
+++ scrapers/guac.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const platform = "guac";
 module.exports = [platform, async function (id) {
     const {data: _data} = await axios.get(`https://api.guac.live/watch/${id}`);
-    if (!_data?.success) return {};
+    if (_data?.statusCode !== 200) return {};
     const data = _data.data;
     return {
         live: data.live,
