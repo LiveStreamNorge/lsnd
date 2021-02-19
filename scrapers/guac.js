b8a0333595d201a7fb3a8a8187c803ff22f1e29e
Thomas Lekanger <datagutt@lekanger.no>
2021-02-19 15:05:33 +0100

    Support guac.live platform

---
+++ scrapers/guac.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const platform = "guac";
+module.exports = [platform, async function (id) {
+    const {data: _data} = await axios.get(`https://api.guac.live/watch/${id}`);
+    if (!_data?.success) return {};
+    const data = _data.data;
+    return {
+        live: data.live,
+        name: data.name,
+        avatar: data.user.avatar,
+        id, platform,
+        viewers: data.viewers,
+        title: data.title,
+    };
+}];
