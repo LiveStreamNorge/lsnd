7f2e07c2e8967fe309a55bc78c627e3d74258543
Thomas Lekanger <thomas.lekanger@hotmail.com>
2022-07-15 11:11:45 +0200

    Update angelthump.js

--- scrapers/angelthump.js
+++ scrapers/angelthump.js
@@ -5,12 +5,12 @@ module.exports = [platform, async function (username) {
     const {data} = await axios.get(`https://api.angelthump.com/v3/streams/?username=${username}`);
     const {data: userData} = await axios.get(`https://api.angelthump.com/v3/users/?username=${username}`);
     return {
-        live: daaa?.[0]?.type === 'live',
+        live: data?.[0]?.type === 'live',
         name: userData?.[0]?.display_name,
-        broadcaster_type: userDataa?.[0]?.type,
-        avatar: userDataa?.[0]?.profile_logo_url,
+        broadcaster_type: userData?.[0]?.type,
+        avatar: userData?.[0]?.profile_logo_url,
         username, platform,
-        viewers: data?.[0]?viewer_count,
+        viewers: data?.[0]?.viewer_count,
         title: userData?.[0]?.title,
         thumbnail_url: data?.[0]?.thumbnail_url,
     };
