a4acb4146354129a3c959fd9627c3fb2a6a1be70
Thomas Lekanger <thomas.lekanger@hotmail.com>
2022-12-28 17:40:02 +0100

    fix(kick): improve scraping

--- index.js
+++ index.js
@@ -1,83 +1,82 @@
-const http = require('http');
+const http = require("http");
 
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 
-const nconf = require('nconf');
+const nconf = require("nconf");
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 const scrapers = require("./scrapers");
 
-nconf.argv()
-    .env()
-    .file({
-        file: 'config.json'
-    }
-);
+nconf.argv().env().file({
+  file: "config.json",
+});
 
-nconf.set('http:port', '9080');
-nconf.set('http:ssl', false);
-nconf.set('http:ssl_port', '9443');
+nconf.set("http:port", "9080");
+nconf.set("http:ssl", false);
+nconf.set("http:ssl_port", "9443");
 
-nconf.set('http:ssl_cert', '');
-nconf.set('http:ssl_privkey', '');
+nconf.set("http:ssl_cert", "");
+nconf.set("http:ssl_privkey", "");
 nconf.save();
 
-const port = nconf.get('http:port') || 80;
-const sslPort = nconf.get('http:ssl_port') || 443;
+const port = nconf.get("http:port") || 80;
+const sslPort = nconf.get("http:ssl_port") || 443;
 const app = express();
 
 const idToData = new Map();
 
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
-    windowMs: 60 * 1000, // 1 minute
-    max: 50 // limit each IP to 100 requests per windowMs
+  windowMs: 60 * 1000, // 1 minute
+  max: 50, // limit each IP to 100 requests per windowMs
 });
 
 app.use(cors());
 app.use(limiter);
 
-app.get('/streams', async (req, res) => {
-    let data = Array.from(idToData.values());
-    if(req.query.filter && typeof req.query.filter === 'object'){
-        let filteredData = data;
-        Object.entries(req.query.filter).forEach(arg => {
-            var index = arg[0];
-            var filter = arg[1];
-            filteredData = filteredData.filter(d => {
-                if(filter === 'true') filter = true;
-                if(filter === 'false') filter = false;
-                if(filter && typeof filter === 'string' && filter.indexOf(',') > -1){
-                    filters = filter.split(',');
-                    for(f in filters){
-                        if(filters.hasOwnProperty(f)){
-                            if(d[index] === filters[f]) return true;
-                        }
-                    }
-                    return false;
-                }else{
-                    return d[index] === filter;
-                }
-            })
-        })
-        res.send(filteredData);
-    }else{
-        res.send(data);
-    }
-    console.info(`[${req.ip}] Requested /streams`);
+app.get("/streams", async (req, res) => {
+  let data = Array.from(idToData.values());
+  if (req.query.filter && typeof req.query.filter === "object") {
+    let filteredData = data;
+    Object.entries(req.query.filter).forEach((arg) => {
+      var index = arg[0];
+      var filter = arg[1];
+      filteredData = filteredData.filter((d) => {
+        if (filter === "true") filter = true;
+        if (filter === "false") filter = false;
+        if (filter && typeof filter === "string" && filter.indexOf(",") > -1) {
+          filters = filter.split(",");
+          for (f in filters) {
+            if (filters.hasOwnProperty(f)) {
+              if (d[index] === filters[f]) return true;
+            }
+          }
+          return false;
+        } else {
+          return d[index] === filter;
+        }
+      });
+    });
+    res.send(filteredData);
+  } else {
+    res.send(data);
+  }
+  console.info(`[${req.ip}] Requested /streams`);
 });
 
-app.get('/platforms', (req, res) => {
-   res.send([...scrapers]?.map(s => s[0]) ?? {});
+app.get("/platforms", (req, res) => {
+  res.send([...scrapers]?.map((s) => s[0]) ?? {});
 });
 
-app.get('/src', (req, res) => {
-    res.send(`Copyright ${new Date().getFullYear()}, AGPLv3, https://github.com/LiveStreamNorge/lsnd`)
+app.get("/src", (req, res) => {
+  res.send(
+    `Copyright ${new Date().getFullYear()}, AGPLv3, https://github.com/LiveStreamNorge/lsnd`
+  );
 });
 
 const updatePeriod = 5 * 60 * 1000;
@@ -86,86 +85,96 @@ const updatePeriod = 5 * 60 * 1000;
  * @return Boolean
  * @returns Return true on successful scrape
  */
-async function scrape({platform, userId, customUsername, ...rest}) {
-    let data;
+async function scrape({ platform, userId, customUsername, ...rest }) {
+  let data;
 
-    const scraper = (scrapers.has(platform) && scrapers.get(platform))
-        || ( async () => { throw new Error(`Platform ${platform} not supported!`); } );
-    const id = crypto.createHash('sha256').update(platform + userId + customUsername).digest('hex');
+  const scraper =
+    (scrapers.has(platform) && scrapers.get(platform)) ||
+    (async () => {
+      throw new Error(`Platform ${platform} not supported!`);
+    });
+  const id = crypto
+    .createHash("sha256")
+    .update(platform + userId + customUsername)
+    .digest("hex");
 
-    try {
-        data = await scraper(userId, customUsername);
-    } catch (e) {
-        console.error(`Couldn't scrape ${userId} ${customUsername ?? ""}: `, e.message);
-        // Add a placeholder user (in case the first API request for this user fails)
-        if (!idToData.get(id)) {
-            idToData.set(id, {
-                id,
-                userId,
-                customUsername,
-                featuredRank: rest.featuredRank ?? null,
-                team: rest.team ?? null
-            });
-        }
-        return false;
-    }
-
-    // Append `id' and `userId' fields before adding to the map
-    data = { id, userId, ...data };
-    if(rest.featuredRank) data.featuredRank = rest.featuredRank;
-    if(rest.team) data.team = rest.team;
-
-    idToData.set(id, data);
-
-    return true;
-}
-
-const timestamp = () => new Date().toTimeString().split(' ')[0];
-const loadPeople = async people => {
-    console.info("Populating scrape data...");
-
-    // multithread all initial scrapes, wait for them all to finish
-    await Promise.all(
-        people.map(async (person, i) => {
-            setTimeout(() => {
-                setInterval(async () => {
-
-                    await scrape(person);
-                    console.info(
-                        `[${timestamp()}] Rescraped ${(person.userId)}`
-                    );
-
-                }, updatePeriod);
-            }, (updatePeriod / people.length) * i);
-            // Split `updatePeriod` into equal periods, and then scrape every `updatePeriod`,
-            // so that the scrapes are evenly distributed over the `updatePeriod`.
-
-            await scrape(person) && console.info(`Scraped ${(person.userId)}!`);
-        })
+  try {
+    data = await scraper(userId, customUsername);
+  } catch (e) {
+    console.error(
+      `Couldn't scrape ${userId} ${customUsername ?? ""}: `,
+      e.message
     );
+    // Add a placeholder user (in case the first API request for this user fails)
+    if (!idToData.get(id)) {
+      idToData.set(id, {
+        id,
+        userId,
+        customUsername,
+        featuredRank: rest.featuredRank ?? null,
+        team: rest.team ?? null,
+      });
+    }
+    return false;
+  }
 
-    console.info("Finished scraping everyone!")
+  // Append `id' and `userId' fields before adding to the map
+  data = { id, userId, ...data };
+  if (rest.featuredRank) data.featuredRank = rest.featuredRank;
+  if (rest.team) data.team = rest.team;
+
+  idToData.set(id, data);
+
+  return true;
 }
 
-const people = require('./people.json');
+const timestamp = () => new Date().toTimeString().split(" ")[0];
+const loadPeople = async (people) => {
+  console.info("Populating scrape data...");
+
+  // multithread all initial scrapes, wait for them all to finish
+  await Promise.all(
+    people.map(async (person, i) => {
+      setTimeout(() => {
+        setInterval(async () => {
+          await scrape(person);
+          console.info(`[${timestamp()}] Rescraped ${person.userId}`);
+        }, updatePeriod);
+      }, (updatePeriod / people.length) * i);
+      // Split `updatePeriod` into equal periods, and then scrape every `updatePeriod`,
+      // so that the scrapes are evenly distributed over the `updatePeriod`.
+
+      (await scrape(person)) && console.info(`Scraped ${person.userId}!`);
+    })
+  );
+
+  console.info("Finished scraping everyone!");
+};
+
+const people = require("./people.json");
 
 const httpServer = http.createServer(app);
 httpServer.listen(port, async () => {
-    console.log(`lsnd listening to 0.0.0.0:${port}`);
-    await loadPeople(people);
+  console.log(`lsnd listening to 0.0.0.0:${port}`);
+  await loadPeople(people);
 });
 
 // Try setting up an https server
-if(nconf.get('http:ssl') === true){
-    try {
-        const key = fs.readFileSync(nconf.get('http:ssl_privkey') || '/etc/certs/lsn-api.dat.cloud/privkey.pem');
-        const cert = fs.readFileSync(nconf.get('http:ssl_cert') || '/etc/certs/lsn-api.dat.cloud/fullchain.pem');
-        const httpsServer = https.createServer({key, cert}, app);
-        httpsServer.listen(443, () => {
-            console.log(`lsnd/TLS listening to 0.0.0.0:${sslPort}`);
-        });
-    } catch (e) {
-        console.error("Couldn't set up HTTPS server!");
-        console.error(e.message);
-    }
+if (nconf.get("http:ssl") === true) {
+  try {
+    const key = fs.readFileSync(
+      nconf.get("http:ssl_privkey") ||
+        "/etc/certs/lsn-api.dat.cloud/privkey.pem"
+    );
+    const cert = fs.readFileSync(
+      nconf.get("http:ssl_cert") || "/etc/certs/lsn-api.dat.cloud/fullchain.pem"
+    );
+    const httpsServer = https.createServer({ key, cert }, app);
+    httpsServer.listen(443, () => {
+      console.log(`lsnd/TLS listening to 0.0.0.0:${sslPort}`);
+    });
+  } catch (e) {
+    console.error("Couldn't set up HTTPS server!");
+    console.error(e.message);
+  }
 }
--- scrapers/kick.js
+++ scrapers/kick.js
@@ -1,16 +1,46 @@
-const axios = require("axios");
+const Hero = require("@ulixee/hero-playground");
 
 const platform = "kick";
-module.exports = [platform, async function (username) {
-	const {data} = await axios.get(`https://kick.com/api/v1/channels/${username}`);
-	return {
-		live: data?.livestream?.is_live,
-		name: data?.name,
-		broadcaster_type: data?.role, // TODO
-		avatar: data?.user?.profile_pic,
-		username, platform,
-		viewers: data?.livestream?.viewers,
-		title: data?.livestream?.session_title, // or session_description
-		thumbnail_url: data?.livestream?.thumbnail?.url,
-	};
-}];
+module.exports = [
+  platform,
+  async function (username) {
+    const hero = new Hero();
+    await hero.goto(`https://kick.com`, {
+      timeoutMs: 10000,
+    });
+    const response = await hero.fetch(
+      `https://kick.com/api/v1/channels/${username}`,
+      {
+        method: "GET",
+      }
+    );
+    let data = {};
+    if (await response.ok) {
+      data = await response.json();
+    }
+
+    if (!data) {
+      return {
+        name: username,
+        avatar,
+        live: false,
+        title: null,
+        platform,
+        viewers: null,
+        thumbnail_url: null,
+      };
+    }
+
+    return {
+      live: data?.livestream && data?.livestream?.is_live ? true : false,
+      name: data?.user?.slug,
+      broadcaster_type: data?.verified ? "PARTNER" : "",
+      avatar: data?.user?.profile_pic,
+      username,
+      platform,
+      viewers: data?.livestream?.viewers,
+      title: data?.livestream?.session_title, // or session_description
+      thumbnail_url: data?.livestream?.thumbnail?.url,
+    };
+  },
+];
