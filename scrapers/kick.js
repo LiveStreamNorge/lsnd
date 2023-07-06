3ae75b1c7d080f11f67e803e0b94caf798f48fcc
Thomas Lekanger <thomas.lekanger@hotmail.com>
2023-07-06 02:41:05 +0200

    fix(kick): improve kick support

--- scrapers/kick.js
+++ scrapers/kick.js
@@ -1,15 +1,49 @@
-const axios = require("axios");
 const platform = "kick";
 const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
-const https = require("https");
-const httpsAgent = new https.Agent({
-  maxVersion: "TLSv1.3",
-  minVersion: "TLSv1.3"
-});
+const initCycleTLS = require("cycletls");
+
+let xsrfToken = null;
+
+// get xsrf token from cookie
+async function fetchXSFRToken() {
+  // Initiate CycleTLS
+  const cycleTLS = await initCycleTLS();
+
+  const request = await cycleTLS.head(`https://${platform}.com`, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "User-Agent": `${capitalize(
+        platform
+      )}/1.0.13 Dalvik/2.1.0 (Linux; U; Android 13; Pixel 6 Pro Build/TQ1A.221205.011)`,
+    },
+    ja3: "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
+    method: "GET",
+  });
+  const cookies = request.headers["Set-Cookie"].join(";");
+  if (!cookies) {
+    console.error("No cookies");
+    return false;
+  }
+  const xsrfCookie = cookies
+    .split(";")
+    .find((cookie) => cookie.includes("XSRF-TOKEN"));
+  if (!xsrfCookie) {
+    console.error("No XSRF-TOKEN cookie");
+    return false;
+  }
+  xsrfToken = xsrfCookie.split("=")[1];
+  return true;
+}
+
+fetchXSFRToken();
+
 module.exports = [
   platform,
   async function (username) {
-    const { data } = await axios.get(
+    // Initiate CycleTLS
+    const cycleTLS = await initCycleTLS();
+    const res = await cycleTLS.get(
       `https://${platform}.com/api/v2/channels/${username}`,
       {
         headers: {
@@ -19,9 +53,20 @@ module.exports = [
             platform
           )}/1.0.13 Dalvik/2.1.0 (Linux; U; Android 13; Pixel 6 Pro Build/TQ1A.221205.011)`,
         },
-        httpsAgent
+        Cookies: [
+          {
+            name: "XSRF-TOKEN",
+            value: xsrfToken,
+            domain: "kick.com",
+            path: "/",
+            secure: true,
+            httpOnly: true,
+          },
+        ],
+        ja3: "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
       }
     );
+    const data = res?.body;
 
     if (!data) {
       return {
@@ -37,7 +82,11 @@ module.exports = [
     return {
       live: data?.livestream && data?.livestream?.is_live ? true : false,
       name: data?.user?.username,
-      broadcaster_type: data?.verified ? "PARTNER" : "",
+      broadcaster_type: data?.verified
+        ? "PARTNER"
+        : data?.subscription_enabled
+        ? "affiliate"
+        : "",
       avatar: data?.user?.profile_pic,
       username,
       platform,
